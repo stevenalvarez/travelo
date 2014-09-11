04d46a0292653312190fe9e883e5c5dc942a3c44
jhonny <steven.alvarez.v@gmail.com>
2014-09-11 19:11:54 -0400

    Destination City

---
+++ app/webroot/html/destination_city.php
@@ -0,0 +1,252 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+    	<meta http-equiv="content-type" content="text/html" />
+    	<meta name="author" content="jhonny" />
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    	<title>Destination City</title>
+        
+        <link rel="stylesheet" type="text/css" href="css/styles.css"/>
+        <!-- include slider.css -->
+        <link rel="stylesheet" type="text/css" href="css/owl.carousel.css"/>
+        <link rel="stylesheet" type="text/css" href="css/owl.theme.css"/>
+        <link rel="stylesheet" type="text/css" href="css/jquery.magnific-popup.css"/>
+        <link rel="stylesheet" type="text/css" href="css/jquery.custom.file.upload.css"/>
+        <link rel="stylesheet" type="text/css" href="css/jquery-ui.min.css"/>
+        <link rel="stylesheet" type="text/css" href="css/component.css" />
+        <link rel="stylesheet" type="text/css" href="css/main.css"/>
+        
+        <script type="text/javascript" src="js/jquery-1.9.1.js"></script>
+        <script type="text/javascript" src="js/jquery-ui.min.js"></script>
+        <script type="text/javascript" src="js/jquery.customSelect.min.js"></script>
+        <script type="text/javascript" src="js/jquery.custom.file.upload.js"></script>
+        <script type="text/javascript" src="js/jquery.magnific-popup.min.js"></script>
+        <script type="text/javascript" src="js/owl.carousel.min.js"></script>
+        <script type="text/javascript" src="js/jquery.flash.min_.js"></script>
+        <script type="text/javascript" src="js/modernizr.custom.js"></script>
+        <script type="text/javascript" src="js/nlform.js"></script>
+        <script type="text/javascript" src="js/main.js"></script>
+        
+    </head>
+    <body>
+    	<div id="container">
+    		<?php include("includes/header.php"); ?>
+    		<div id="content" class="page">
+                <div class="inner overflow">
+                    <div class="cols2 left">
+                        <ul class="blogs stories destination">
+                            <li>
+                                <div class="media">
+                                    <div class="data">
+                                        <img src="img/default_blue.png" />
+                                    </div>
+                                </div>
+                                <div class="description padding20 bwhite">
+                                    <div class="text">
+                                        <p><b class="vowel2 left">B</b> <span class="colorblue">ighton</span>, the best ciry......usto eget diam posuere sollicitudin eu tincidunt nulla. Curabitur eleifend tempor magna, in scelerisque urna placerat vel. Phasellus eget sem id sto consequat egestas quis facilisis metus. justo eget diam posuere sollicitudin eu tincidunt nulla. Curabitur eleifend tempor dfdfdmagna, in Sto eget diam posuere sollicitudin eu tincidunt nulla. Curabitur eleifend tempor magna, in scelerisque urna placerat vel. Phasellus eget sem id sto consequat egestas quis facilisis metus. justo eget diam posuere sollicitudin eu tincidunt nulla. Curabitur eleifend tempor magna, in scelerisque urna placerat vel. Phasellus eget sem id sto consequat egestas quis facilisis metus. justo eget diam posuereusto eget diam posuere sollicitudin eu tincidunt nulla. Curabitur eleifend tempor magna, in scelerisque urna placerat vel. Phasellus eget sem id sto consequat egestas quis facilisis metus. justo eget diam posuere sollicitudin eu tincidunt nulla. Curabitur eleifend tempor magna, in scelerisque urna placerat vel. Phasellus eget sem id sto consequat egestas quis facilisis metus. justo eget diam posuere</p>
+                                        <p>The best ciry......usto eget diam posuere sollicitudin eu tincidunt nulla. Curabitur eleifend tempor magna, in scelerisque urna placerat vel. Phasellus eget sem id sto consequat egestas quis facilisis metus. justo eget diam posuere sollicitudin eu tincidunt nulla. Curabitur eleifend tempor magna, in scelerisque urna placerat vel. Phasellus eget sem id sto consequat egestas quis facilisis metus. justo eget diam posuere Sto eget diam posuere sollicitudin eu tincidunt nulla. Curabitur eleifend tempor magna, in scelerisque urna placerat vel. Phasellus eget sem id sto consequat egestas quis facilisis metus. justo eget diam posuere sollicitudin eu tincidunt nulla. Curabitur eleifend tempor magna, in scelerisque urna placerat vel. Phasellus eget sem id sto consequat egestas quis facilisis metus. justo eget diam posuere usto eget diam posuere sollicitudin eu tincidunt nulla. Curabitur eleifend tempor magna, in scelerisque urna placerat vel. Phasellus eget sem id sto consequat egestas quis facilisis metus. justo eget diam posuere sollicitudin eu tincidunt nulla. Curabitur eleifend tempor magna, in scelerisque urna placerat vel. Phasellus eget sem id sto consequat egestas quis facilisis metus. justo eget diam po..er......</p>
+                                        <a class="right more" href="#">Leer m&aacute;s</a>
+                                    </div>
+                                </div>
+                            </li>
+                            <li class="title_destination">
+                                <h2>Expats  Experiencias en Brighton</h2>
+                            </li>
+                            <li>
+                                <div class="details overflow bwhite">
+                                    <div class="left padding20 box">
+                                        <img src="img/stories1.jpg"/>
+                                        <h3>Jessica Brown</h3>
+                                    </div>
+                                    <div class="right padding20 descripcion">
+                                        <h4>Presentacion en London</h4>
+                                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's stand dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.</p>
+                                    </div>
+                                </div>
+                                <div class="bsocial padding10 bwhite overflow">
+                                    <div class="reviews">
+                                        <div class="left">
+                                            <span class="have_comments">
+                                                <span class="bold">3 Comentarios</span>
+                                            </span>
+                                        </div>
+                                        <ul class="right">
+                                            <li>
+                                                <a class="likes" href="#"> 22 likes</a>
+                                            </li>
+                                            <li class="last">
+                                                <a class="date" href="#"> 11 NOV 2013</a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="details overflow bwhite">
+                                    <div class="left padding20 box">
+                                        <img src="img/stories3.jpg"/>
+                                        <h3>Kyle Martin</h3>
+                                    </div>
+                                    <div class="right padding20 descripcion">
+                                        <h4>Presentación, Hola Londinenses!</h4>
+                                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's stand dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.</p>
+                                    </div>
+                                </div>
+                                <div class="bsocial padding10 bwhite overflow">
+                                    <div class="reviews">
+                                        <div class="left">
+                                            <span class="have_comments">
+                                                <span class="bold">3 Comentarios</span>
+                                            </span>
+                                        </div>
+                                        <ul class="right">
+                                            <li>
+                                                <a class="likes" href="#"> 22 likes</a>
+                                            </li>
+                                            <li class="last">
+                                                <a class="date" href="#"> 11 NOV 2013</a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="details overflow bwhite">
+                                    <div class="left padding20 box">
+                                        <img src="img/stories4.jpg"/>
+                                        <h3>David Jackson</h3>
+                                    </div>
+                                    <div class="right padding20 descripcion">
+                                        <h4>Perfect budget hotel very clean</h4>
+                                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's stand dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.</p>
+                                        <h4>2 Helpful Things</h4>
+                                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's stand dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.</p>
+                                    </div>
+                                </div>
+                                <div class="bsocial padding10 bwhite overflow">
+                                    <div class="reviews">
+                                        <div class="left">
+                                            <span class="have_comments">
+                                                <span class="bold">3 Comentarios</span>
+                                            </span>
+                                        </div>
+                                        <ul class="right">
+                                            <li>
+                                                <a class="likes" href="#"> 22 likes</a>
+                                            </li>
+                                            <li class="last">
+                                                <a class="date" href="#"> 11 NOV 2013</a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="details overflow bwhite">
+                                    <div class="left padding20 box">
+                                        <img src="img/stories5.jpg"/>
+                                        <h3>David Jackson</h3>
+                                    </div>
+                                    <div class="right padding20 descripcion">
+                                        <h4>We had Fantastic Adventure in Paris</h4>
+                                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's stand dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.</p>
+                                        <div class="pictures">
+                                            <h5>My Travel Photos</h5>
+                                            <ul class="overflow">
+                                                <li>
+                                                    <a href="#">
+                                                        <img src="img/spicture7.jpg" />
+                                                        <img class="hover" src="img/shover.png" />
+                                                    </a>
+                                                </li>
+                                                <li>
+                                                    <a href="#">
+                                                        <img src="img/spicture8.jpg" />
+                                                        <img class="hover" src="img/shover.png" />
+                                                    </a>
+                                                </li>
+                                                <li>
+                                                    <a href="#">
+                                                        <img src="img/spicture9.jpg" />
+                                                        <img class="hover" src="img/shover.png" />
+                                                    </a>
+                                                </li>
+                                                <li>
+                                                    <a href="#">
+                                                        <img src="img/spicture10.jpg" />
+                                                        <img class="hover" src="img/shover.png" />
+                                                    </a>
+                                                </li>
+                                                <li>
+                                                    <a href="#">
+                                                        <img src="img/spicture11.jpg" />
+                                                        <img class="hover" src="img/shover.png" />
+                                                    </a>
+                                                </li>
+                                                <li>
+                                                    <a href="#">
+                                                        <img src="img/spicture12.jpg" />
+                                                        <img class="hover" src="img/shover.png" />
+                                                    </a>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="bsocial padding10 bwhite overflow">
+                                    <div class="reviews">
+                                        <div class="left">
+                                            <span class="have_comments">
+                                                <span class="bold">3 Comentarios</span>
+                                            </span>
+                                        </div>
+                                        <ul class="right">
+                                            <li>
+                                                <a class="likes" href="#"> 22 likes</a>
+                                            </li>
+                                            <li class="last">
+                                                <a class="date" href="#"> 11 NOV 2013</a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="details overflow bwhite">
+                                    <div class="left padding20 box">
+                                        <img src="img/stories6.jpg"/>
+                                        <h3>Jessica Brown</h3>
+                                    </div>
+                                    <div class="right padding20 descripcion">
+                                        <h4>Best Adventure Trip ever with Expats</h4>
+                                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's stand dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries....</p>
+                                    </div>
+                                </div>
+                                <div class="bsocial padding10 bwhite overflow">
+                                    <div class="reviews">
+                                        <div class="left">
+                                            <span class="have_comments">
+                                                <span class="bold">3 Comentarios</span>
+                                            </span>
+                                        </div>
+                                        <ul class="right">
+                                            <li>
+                                                <a class="likes" href="#"> 22 likes</a>
+                                            </li>
+                                            <li class="last">
+                                                <a class="date" href="#"> 11 NOV 2013</a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                        <a href="#" class="load_more">VER MAS EXPERIENCIAS</a>
+                    </div><!--.cols2-->
+                    <?php include("includes/sidebar_destination_city.php"); ?>
+                </div><!--.inner-->
+    		</div><!--#content-->
+            <?php include("includes/footer.php"); ?>
+    	</div>
+    </body>
+</html>
---
+++ app/webroot/html/includes/sidebar_destination_city.php
@@ -0,0 +1,159 @@
+<div class="widget right">
+    <div class="column mbttom30 bwhite oncity">
+        <h4>SOBRE LA CIUDAD</h4>
+        <ul class="options">
+            <li>
+                <a href="#" title="Trabajo">TRABAJO</a>
+            </li>
+            <li>
+                <a href="#" title="Alojamiento">ALOJAMIENTO</a>
+            </li>
+            <li>
+                <a href="#" title="Idiomas en el extranjero">IDIOMAS EN EL EXTRANJERO</a>
+            </li>
+            <li class="last">
+                <a href="#" title="Foro">FORO</a>
+            </li>
+        </ul>
+    </div>
+    <div class="column mbttom30 flag">
+        <div class="top">
+            <div class="cols left">
+                <div class="box">
+                    <img src="img/flag.jpg" />
+                </div>
+            </div>
+            <div class="cols right">
+                <ul class="info">
+                    <li>
+                        <span>11.090 usuarios</span>
+                    </li>
+                    <li>
+                        <span>120 comentarios</span>
+                    </li>
+                    <li>
+                        <span>13000 expats están viviendo en Brighton</span>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="bottom clear">
+            <div class="reviews">
+                <div class="stars">
+                    <span class="star selected">1</span>
+                    <span class="star selected">2</span>
+                    <span class="star selected">3</span>
+                    <span class="star selected">4</span>
+                    <span class="star">5</span>
+                </div>
+                <span>270 Reviews de expats</span>
+            </div>
+        </div>
+        <br class="clear" />
+        <a class="btn orange uppercase" href="javascript:void(0)" title="Seguir Brighton">Seguir Brighton</a>
+    </div>
+    <div class="column popular mbttom30 overflow">
+        <h5>HOT  in Brighton</h5>
+        <ul class="clear options">
+            <li>
+                <a href="javascript:void(0)" title="Alojamiento" lang="alojamiento">ALOJAMIE...</a>
+            </li>
+            <li>
+                <a class="selected" href="javascript:void(0)" title="Escuelas" lang="escuelas">ESCUELAS</a>
+            </li>
+            <li class="last">
+                <a class="last" href="javascript:void(0)" title="Trabajo" lang="trabajo">TRABAJO</a>
+            </li>
+        </ul>
+        <div class="list_popular result destination padding20 bwhite overflow clear">
+            <ul id="alojamiento">
+            </ul>
+            <ul id="escuelas" class="selected">
+                <li>
+                    <div class="left imagen">
+                        <img src="img/default_gray.jpg" />
+                    </div>
+                    <div class="right description">
+                        <h5>ISE ItNGLES</h5>
+                        <div class="stars">
+                            <span class="star selected">1</span>
+                            <span class="star selected">2</span>
+                            <span class="star selected">3</span>
+                            <span class="star selected">4</span>
+                            <span class="star">5</span>
+                        </div>
+                        <span class="reviews">270 Reviews de expats</span>
+                    </div>
+                </li>
+                <li>
+                    <div class="left imagen">
+                        <img src="img/default_gray.jpg" />
+                    </div>
+                    <div class="right description">
+                        <h5>AIL BRIGHTON</h5>
+                        <div class="stars">
+                            <span class="star selected">1</span>
+                            <span class="star selected">2</span>
+                            <span class="star selected">3</span>
+                            <span class="star selected">4</span>
+                            <span class="star">5</span>
+                        </div>
+                        <span class="reviews">270 Reviews de expats</span>
+                    </div>
+                </li>
+                <li>
+                    <div class="left imagen">
+                        <img src="img/default_gray.jpg" />
+                    </div>
+                    <div class="right description">
+                        <h5>ESE</h5>
+                        <div class="stars">
+                            <span class="star selected">1</span>
+                            <span class="star selected">2</span>
+                            <span class="star selected">3</span>
+                            <span class="star selected">4</span>
+                            <span class="star">5</span>
+                        </div>
+                        <span class="reviews">270 Reviews de expats</span>
+                    </div>
+                </li>
+                <li>
+                    <div class="left imagen">
+                        <img src="img/default_gray.jpg" />
+                    </div>
+                    <div class="right description">
+                        <h5>AIL BRIGHTON</h5>
+                        <div class="stars">
+                            <span class="star selected">1</span>
+                            <span class="star selected">2</span>
+                            <span class="star selected">3</span>
+                            <span class="star selected">4</span>
+                            <span class="star">5</span>
+                        </div>
+                        <span class="reviews">270 Reviews de expats</span>
+                    </div>
+                </li>
+                <li class="last">
+                    <div class="left imagen">
+                        <img src="img/default_gray.jpg" />
+                    </div>
+                    <div class="right description">
+                        <h5>ESE</h5>
+                        <div class="stars">
+                            <span class="star selected">1</span>
+                            <span class="star selected">2</span>
+                            <span class="star selected">3</span>
+                            <span class="star selected">4</span>
+                            <span class="star">5</span>
+                        </div>
+                        <span class="reviews">270 Reviews de expats</span>
+                    </div>
+                </li>
+            </ul>
+            <ul id="trabajo">
+            </ul>
+            <br class="clear" />
+            <a class="lmore btn green" href="javascript:void(0)" title="MORE">MORE</a>
+        </div>
+    </div>    
+</div><!--.sidebar_destination_city-->
