6acc5a86dc021101a0432eb66f57cb726b2a1ac5
jhonny <steven.alvarez.v@gmail.com>
2014-09-16 12:02:58 -0400

    Experiences

---
+++ app/webroot/html/includes/sidebar_experiences.php
@@ -0,0 +1,90 @@
+<div class="widget right">
+    <div class="column mbttom30 bwhite oncity">
+        <h4>Foros en Brighton</h4>
+        <ul class="options">
+            <li>
+                <a href="#" title="Au Pair">
+                    <span class="title">AU PAIR</span>
+                    <span class="right fontsize11">72 POST</span>
+                </a>
+            </li>
+            <li>
+                <a href="#" title="Alojamiento">
+                    <span class="title">ALOJAMIENTO</span>
+                    <span class="right fontsize11">722 POST</span>
+                </a>
+            </li>
+            <li>
+                <a href="#" title="Bancos">
+                    <span class="title">BANCOS</span>
+                    <span class="right fontsize11">122 POST</span>
+                </a>
+            </li>
+            <li>
+                <a href="#" title="Contactos">
+                    <span class="title">CONTACTOS</span>
+                    <span class="right fontsize11">143 POST</span>
+                </a>
+            </li>
+            <li>
+                <a href="#" title="Dudas Legales">
+                    <span class="title">DUDAS LEGALES</span>
+                    <span class="right fontsize11">120 POST</span>
+                </a>
+            </li>
+            <li>
+                <a href="#" title="Estudiar Idiomas">
+                    <span class="title">ESTUDIAR IDIOMAS</span>
+                    <span class="right fontsize11">720 POST</span>
+                </a>
+            </li>
+            <li>
+                <a href="#" title="Fimilia e Hijos">
+                    <span class="title">FAMILIA E HIJOS</span>
+                    <span class="right fontsize11">740 POST</span>
+                </a>
+            </li>
+            <li>
+                <a class="selected" href="#" title="Presentaciones">
+                    <span class="title">PRESENTACIONES</span>
+                    <span class="right fontsize11">127 POST</span>
+                </a>
+            </li>
+            <li>
+                <a href="#" title="Sanidad">
+                    <span class="title">SANIDAD</span>
+                    <span class="right fontsize11">230 POST</span>
+                </a>
+            </li>
+            <li>
+                <a href="#" title="Vida Diaria">
+                    <span class="title">VIDA DIARIA</span>
+                    <span class="right fontsize11">443 POST</span>
+                </a>
+            </li>
+            <li class="last">
+                <a href="#" title="Visados y Embajadas">
+                    <span class="title">VISADOS Y EMBAJADAS</span>
+                    <span class="right fontsize11">433 POST</span>
+                </a>
+            </li>
+        </ul>
+    </div>
+    <div class="column mbttom30 bwhite oncity">
+        <h4>Experiencias Relacionados</h4>
+        <ul class="related">
+            <li>
+                <a href="#" title="Primera vez entre vosotros!">Primera vez entre vosotros!</a>
+            </li>
+            <li>
+                <a href="#" title="Por aqui me presento">Por aqui me presento</a>
+            </li>
+            <li>
+                <a href="#" title="Hola buenas!">Hola buenas!</a>
+            </li>
+            <li class="last">
+                <a href="#" title="Mi primera entrada en el foro">Mi primera entrada en el foro</a>
+            </li>
+        </ul>
+    </div>
+</div><!--.sidebar_experiences-->
